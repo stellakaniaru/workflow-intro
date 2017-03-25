c1deb0b00ad9a53ceeaec3472e296ccbf8203a7c
Stella Kaniaru <stellakaniaru16@gmail.com>
2017-03-26 02:21:39 +0300

    Implementation of tryCatch block code

--- issue-003/solution.js
+++ issue-003/solution.js
@@ -1,7 +1,14 @@
 function product(a, b) {
-	if ((typeof a === 'number') && (typeof b === 'number')) {
-		return a * b;
-	} else {
-		throw new Error('NaN Expection');
+	try {
+		if ((typeof a !== 'number') || (typeof b !== 'number')) {
+			throw new Error('NaN Exception');
+		}
+		else {
+			return a * b;
+		} 
 	}
+	catch(e) {
+		throw e;
+	}
+	
 }
